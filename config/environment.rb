9465d3fd03254948ee8d40932ddce21be90b7dbc
Eugene Awot <easyable01@gmail.com>
2011-10-21 14:29:34 -0400

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Johnmck::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-   :address => "smtp.gmail.com", 
-   :enable_starttls_auto => true, 
-   :port => 587, 
-   :authentication => :plain, 
-   :user_name => "easyable01@johnjmckelvie.com", :password => 'Nancy123'
- }
---
+++ config/initializers/smtp_gmail.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com", 
+  :enable_starttls_auto => true, 
+  :port => 587, 
+  :authentication => :plain, 
+  :user_name => "easyable01@johnjmckelvie.com", 
+  :password => 'Nancy123'
+}
